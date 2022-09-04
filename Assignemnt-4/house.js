1dbe72e662e734f20237240234c97744ae3c0f6f
ManivelRamu <98895862+ManivelRamu@users.noreply.github.com>
2022-09-04 13:57:53 +0530

    Add files via upload

---
+++ Assignemnt-4/house.js
@@ -0,0 +1,16 @@
+let num;
+var houses;
+function house(num)
+{
+    if(num>1)
+    {
+        houses=(num*6)-(num-1)
+     console.log(houses);
+    }
+    else if(num=1)
+    {
+     houses=num*6  
+    console.log(houses);
+    }
+}
+house(4);
